ab39232d804190ecbffc8e376e2164b1a49bb4de
Konstantin Zagorulya <k.zagorulya@mail.ru>
2023-10-05 23:41:56 +0600

    Task 2.1.4: Solution #1

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,7 +1,14 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
 public class Timer {
 
+    public static Timer timer = new Timer();
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
